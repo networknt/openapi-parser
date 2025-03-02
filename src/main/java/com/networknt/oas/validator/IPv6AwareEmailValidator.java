c22528d9a2c2b90ee203a16ffb915075a8fd4092
Steve Hu <stevehu@gmail.com>
2025-03-02 17:06:43 -0500

    fixes #67 remove the email validator dependency (#68)

---
+++ src/main/java/com/networknt/oas/validator/IPv6AwareEmailValidator.java
@@ -0,0 +1,30 @@
+package com.networknt.oas.validator;
+
+/**
+ * This is an extension of the Apache Commons Validator that correctly
+ * handles email addresses containing an IPv6 literal as the domain.
+ * <p>
+ * Apache's {@link EmailValidator} delegates validation of the domain to
+ * its {@link DomainValidator}, which is not aware that it is validating
+ * an email address, which has a peculiar way of representing an IPv6
+ * literal.
+ */
+public class IPv6AwareEmailValidator extends EmailValidator {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Creates a new IPv6AwareEmailValidator.
+     *
+     * @param allowLocal Should local addresses be considered valid?
+     * @param allowTld Should TLDs be allowed?
+     */
+    public IPv6AwareEmailValidator(final boolean allowLocal, final boolean allowTld) {
+        super(allowLocal, allowTld);
+    }
+
+    @Override
+    protected boolean isValidDomain(String domain) {
+        return super.isValidDomain(domain.startsWith("[IPv6:") ? domain.replace("IPv6:", "") : domain);
+    }
+
+}
